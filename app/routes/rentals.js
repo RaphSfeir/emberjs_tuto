facf47a92eb0c38f819ad7a2cec75d43d3e5efb7
Raphaël Sfeir <raph.sfeir@gmail.com>
2016-11-04 17:58:47 +0100

    First commit

---
+++ app/routes/rentals.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+
+let rentals = [{
+	id: 'grand-old-manion', 
+	title: 'Grand Old Mansion',
+	owner: 'Jacques Chirac',
+	city: 'Paris',
+	type: 'Estate',
+	bedrooms: 14,
+	image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
+}, 
+{
+	id: 'urban-living',
+	title: 'Urban Living', 
+	owner: 'Maurice Couillon',
+	city: 'Marseille',
+	type: 'Condo',
+	bedrooms: 1,
+	image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg'
+}];
+
+export default Ember.Route.extend({
+	model() {
+		return rentals; 
+	}
+});
